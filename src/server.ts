1ac428f2176946c92f765fb945c99753999ce37a
Erik Grupioni <55812084+ErikGrupioni@users.noreply.github.com>
2021-10-05 07:03:32 -0300

    Update server.ts
    Mudamos para português

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: "Hello world!"});
+  return response.json({message: "ola mundo"});
 });
 
 app.listen(3333, () => console.log("Server is running!")); 
